246b063665c138afbf686cb23c77eb78369fdae6
senluoye <57495136+senluoye@users.noreply.github.com>
2021-09-13 20:48:12 +0800

    又加了几个bug

--- server/src/main/java/com/qks/makerSpace/dao/EnterpriseDao.java
+++ server/src/main/java/com/qks/makerSpace/dao/EnterpriseDao.java
@@ -3,7 +3,11 @@ package com.qks.makerSpace.dao;
 import com.qks.makerSpace.entity.Enterprise;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EnterpriseDao {
     Enterprise getOneEnterpriseById(String id);
+    List<Enterprise> getAllEnterprise();
+    Integer addEnterprise();
 }
