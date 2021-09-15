90a272e326cc72331805d23be11460fddb343ceb
senluoye <57495136+senluoye@users.noreply.github.com>
2021-09-15 20:43:44 +0800

    下次一定

--- makerspace.sql
+++ makerspace.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80022
  File Encoding         : 65001
 
- Date: 15/09/2021 20:32:33
+ Date: 15/09/2021 20:43:25
 */
 
 SET NAMES utf8mb4;
@@ -60,7 +60,7 @@ CREATE TABLE `connect`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `employee`;
 CREATE TABLE `employee`  (
-  `employeeId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `employee_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `employee` int NOT NULL,
   `doctor` int NOT NULL,
   `master` int NOT NULL,
@@ -74,7 +74,7 @@ CREATE TABLE `employee`  (
   `talents` int NOT NULL,
   `trainee` int NOT NULL,
   `employment` int NOT NULL,
-  PRIMARY KEY (`employeeId`) USING BTREE
+  PRIMARY KEY (`employee_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
