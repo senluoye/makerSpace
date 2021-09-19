42fa6eba0e1881522cc27d7c31ee64027464b93e
Z-Yiheng <1191453751@qq.com>
2021-09-19 22:41:54 +0800

    后端 Activity&Property

--- server/src/main/java/com/qks/makerSpace/dao/ActivityDao.java
+++ server/src/main/java/com/qks/makerSpace/dao/ActivityDao.java
@@ -1,6 +1,20 @@
 package com.qks.makerSpace.dao;
 
+import com.qks.makerSpace.entity.Activity;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
+@Repository
+@Mapper
 public interface ActivityDao {
 
+    Activity getOneActivityById(String id);
+    List<Activity> getAllActivity();
+    Integer addActivity(Map<String,Object> map);
+    Integer updateConnect(String enterpriseId, String activityId);
+    Integer deleteActivity(String id);
+    Integer updateActivity(Map<String,Object> map);
 }
--- server/src/main/java/com/qks/makerSpace/dao/PropertyDao.java
+++ server/src/main/java/com/qks/makerSpace/dao/PropertyDao.java
@@ -1,6 +1,18 @@
 package com.qks.makerSpace.dao;
 
+import com.qks.makerSpace.entity.Property;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public interface PropertyDao {
 
+    Property getPropertyByID(String id);
+    List<Property> getAllProperty();
+    Integer addProperty(Map<String,Object> map);
+    Integer updateConnect(String enterpriseId, String propertyId);
+    Integer deleteProperty(String id);
+    Integer updateProperty(Map<String,Object> map);
 }
